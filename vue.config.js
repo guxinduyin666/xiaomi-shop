5843bd3afdb3511f768d84b1ade7298a4e841b50
wangcc6 <wangcc6@asiainfo.com>
2019-10-18 20:44:04 +0800

    update

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    devServer:{
+        port:8888
+    },
+    pwa:{
+        favicon32: 'favicon.ico',
+        favicon16: 'favicon.ico',
+        appleTouchIcon: 'favicon.ico',
+        maskIcon: 'favicon.ico',
+        msTileImage: 'favicon.ico'
+    }
+}
